32c8e869082b1f35b7df2752424b287a916810eb
Julio Ojeda <jcom.94m@gmail.com>
2020-09-26 13:55:42 -0700

    Configure app.json

--- app.config.js
+++ app.config.js
@@ -1,11 +1,7 @@
 import "dotenv/config";
 
-export default {
-   name: "Expensify",
-   slug: "expensify-app",
-   version: "1.0.0",
-   orientation: "portrait",
-   icon: "./assets/icon.png",
+export default ({ config }) => ({
+   ...config,
    extra: {
       apiKey: process.env.apiKey,
       authDomain: process.env.authDomain,
@@ -15,19 +11,4 @@ export default {
       messagingSenderId: process.env.messagingSenderId,
       appId: process.env.appId,
    },
-   splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
-   },
-   updates: {
-      fallbackToCacheTimeout: 0,
-   },
-   assetBundlePatterns: ["**/*"],
-   ios: {
-      supportsTablet: true,
-   },
-   web: {
-      favicon: "./assets/favicon.png",
-   },
-};
+});
